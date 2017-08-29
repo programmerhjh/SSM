46fffd89c3501667f44b5aa25340444ea87042fe
programmerhjh <605594106@qq.com>
2017-08-29 16:14:49 +0800

    添加服务器以及本地的地址文件，ver-1.0：用于外部服务器

--- src/main/java/tool/GetPropertyUtil.java
+++ src/main/java/tool/GetPropertyUtil.java
@@ -9,6 +9,7 @@ import java.util.Properties;
  */
 public class GetPropertyUtil {
 
+    //根据文件类型获取文件地址
     public static String getFileAddress(String fileType) throws IOException {
         InputStream inStream = new GetPropertyUtil().getFileAddressInputStream();
         Properties pro = new Properties();
@@ -18,10 +19,13 @@ public class GetPropertyUtil {
         return result;
     }
 
+
+    //获取文件地址的输入流
     public InputStream getFileAddressInputStream(){
         return this.getClass().getClassLoader().getResourceAsStream("fileAddress.properties");
     }
 
+    //根据websiteAddress.properties获取不同资源的网址
     public static String getWebsiteAddress(String websiteAddress) throws IOException {
         InputStream inStream = new GetPropertyUtil().getWebsiteAddressInputStream();
         Properties pro = new Properties();
@@ -30,6 +34,7 @@ public class GetPropertyUtil {
         return result;
     }
 
+    //获取资源网址的输入流
     public InputStream getWebsiteAddressInputStream(){
         return this.getClass().getClassLoader().getResourceAsStream("websiteAddress.properties");
     }
